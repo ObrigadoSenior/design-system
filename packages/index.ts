a97874e6ad5af9c650f47907c3200c8761adf145
Johan Sjoberg <johansjoberg88@gmail.com>
2022-09-25 20:34:58 +0200

    update packages

--- packages/index.ts
+++ packages/index.ts
@@ -1,7 +1,4 @@
-export * from './calendar';
 export * from './core';
 export * from './forms';
-export * from './icons';
-export * from './select';
 export * from './system';
 export * from './theme';
--- packages/terminal/src/components/terminal/index.tsx
+++ packages/terminal/src/components/terminal/index.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 import { TerminalProps } from '../../types';
 import styles from './Terminal.module.scss';
 
-export const Terminal = ({}: TerminalProps): JSX.Element => {
+export const Terminal = (): JSX.Element => {
   return <div></div>;
 };
