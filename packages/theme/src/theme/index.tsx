24af9cf648ec7c4ab8423b940903c14f5c3098bb
Johan Sjoberg <johansjoberg88@gmail.com>
2022-09-30 13:46:28 +0200

    Fixed var style name

--- packages/theme/src/theme/index.tsx
+++ packages/theme/src/theme/index.tsx
@@ -5,12 +5,15 @@ import { setDefaultTheme } from './setDefaultTheme';
 import { setFormsTheme } from './setFormsTheme';
 import { setTerminalTheme } from './setTerminalTheme';
 
+const splitUpper = (s: string) => s.match(/([A-Z]?[^A-Z]*)/g)?.slice(0, -1);
+const joinString = (s: string[] = []) => s.join('-').toLowerCase();
+
 export function setStylePerKey<T>(name: string, data: T): void {
   for (const key in data) {
-    const uppercaseSplit = key.match(/([A-Z]?[^A-Z]*)/g)?.slice(0, -1);
-    const joinedKeys = uppercaseSplit?.join('-').toLowerCase();
+    const joinedKeys = joinString(splitUpper(key));
     const value = data[key] as string;
-    const k = `--${name}-${joinedKeys}`;
+    const splitName = joinString(splitUpper(name));
+    const k = `--${splitName}-${joinedKeys}`;
     setStyle(k, value);
   }
 }
